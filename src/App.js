4814607a35811b7d836d2d10382e827ee867c2b6
elikopter <amitai.shalem@gmail.com>
2024-10-26 22:31:51 +0300

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Quiz from './Quiz';
-import Notes from './Notes';
-import MemoryGame from './Memory';
 
 const App = () => {
   return (
